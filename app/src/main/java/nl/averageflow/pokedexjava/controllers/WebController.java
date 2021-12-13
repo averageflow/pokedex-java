e1a7d7e84f7428229960685d17920569abd8013f
Josep Jesus Bigorra Algaba <josep@prowarehouse.nl>
2021-12-13 09:59:11 +0100

    begin adding variables to template

--- app/src/main/java/nl/averageflow/pokedexjava/controllers/WebController.java
+++ app/src/main/java/nl/averageflow/pokedexjava/controllers/WebController.java
@@ -1,13 +1,15 @@
 package nl.averageflow.pokedexjava.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
 
-    @GetMapping(value = "/index")
-    public String index(){
+    @GetMapping({"/", "index"})
+    public String index(Model model){
+        model.addAttribute("myAttribute", "myAttributeValue");
         return "index";
     }
 }
